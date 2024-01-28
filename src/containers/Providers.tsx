92cc1048630b7640dc549a2781ae8c7f43e7432d
Cristian Mhatw <contactomhatw@gmail.com>
2024-01-28 14:15:24 -0500

    chore/Add react-query and react-query-devtools

--- src/containers/Providers.tsx
+++ src/containers/Providers.tsx
@@ -1,9 +1,17 @@
 import { RouterProvider } from "react-router-dom";
 
 import router from "../config/routes.config";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "@/config/reactQuery.config";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const Providers: React.FC = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 };
 
 Providers.defaultProps = {};
